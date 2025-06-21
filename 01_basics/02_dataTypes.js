0834a3241becceed20dc2d507dc8d0aef8cc9b5c
Somya Keshri <keshrisomya9596@gmail.com>
2025-06-21 20:29:06 +0530

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello")   we are using nodejs not browser
+
+console.log(3+3);  // cde readibility should be high
+console.log("Somya")
+
+let name = "Somya"
+let age = 20
+let isLoggedIn = false
+
+// number => 2^53
+// bigint => for very big num beyond number
+// string => '', ""
+// boolean => true/ false
+// null => stands lone value (empty)
+// undefined
+// symbol => unique
+// object
+
+console.log(typeof "Somya");  // string
+console.log(typeof null);   // object
+console.log(typeof undefined);   // undefined
