08447452c8e3e23f1884a2f437e1573122b3258d
Somya Keshri <keshrisomya9596@gmail.com>
2025-06-22 23:44:46 +0530

    Operations

---
+++ 01_basics/03_Operations.js
@@ -0,0 +1,34 @@
+let value = 3
+let negValue = -value
+console.log(negValue);    // -3
+
+// console.log(5+10);    // 15
+// console.log(5-10);    // -5
+// console.log(5*10);    // 50
+// console.log(10/5);    // 2
+// console.log(10%5);    // 0
+// console.log(5**10);   // 9765625 (power)
+// console.log(5/10);    // 0.5
+// console.log(5**2);    // 25 (power)
+
+let str1 = "Hello"
+let str2 = " Somya"
+let str3 = str1 + str2
+console.log(str3);   // Hello Somya
+
+console.log("1" + 2);        // 12
+console.log(1 + "2");        // 12
+console.log("1" + 2 + 2);    // 122
+console.log(1 + 2 + "1");    // 31
+console.log((3+4) * 5 -2);   // 33
+
+console.log(true)        // true
+console.log(+true)       // 1
+console.log(+"")         // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2     // readibility not good
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);     // 101
