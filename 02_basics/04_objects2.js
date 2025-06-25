2fd8a40e29134f5536ec24690517974616c132fc
Somya Keshri <keshrisomya9596@gmail.com>
2025-06-25 15:47:49 +0530

    json talk

---
+++ 02_basics/04_objects2.js
@@ -0,0 +1,29 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+//course.courseInstructor
+const {courseInstructor: instructor} = course     // destructuring
+
+console.log(instructor);        // hitesh
+
+// const navbar = ({company}) => {         // destructure in react
+
+// }
+// navbar(company = "ZS")
+
+
+// JSON format
+// {
+//     "name": "somya",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+// API in array format
+[
+    {},
+    {},
+    {}
+]
