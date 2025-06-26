eb1428f32592ecd40e6bbd1ab0eca3a634ee3c30
Somya Keshri <keshrisomya9596@gmail.com>
2025-06-26 15:14:55 +0530

    Arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -4,14 +4,14 @@ const user = {
 
     welcomeMessage: function(){
         console.log(`${this.username}, welcome to website`);
-        console.log(this)
+        //console.log(this)   // {username.. price.. wlcmMsg..}
     }
 }
-// user.welcomeMessage()      // Buzz, welcome to website
-// user.username = "Sam"
-// user.welcomeMessage()      // Sam, welcome to website
+user.welcomeMessage()      // Buzz, welcome to website
+user.username = "Sam"
+user.welcomeMessage()      // Sam, welcome to website
 
-// console.log(this);         // {}
+console.log(this);         // {}
 
 // function chai(){
 //     let username = "Harry"
@@ -23,14 +23,15 @@ const chai = () => {             // arrow func
     console.log(this.username);        // undefined
     console.log(this);               // {}
 }
-// chai()
+chai()
 
 
 // const addTwo = (num1, num2) => {
 //     return num1 + num2
-// }
-const addTwo = (num1, num2) => num1 + num2
+// } 
+// same thing format different
+const addTwo = (num1, num2) => num1 + num2    // arrow func
 console.log(addTwo(3, 4))         // 7
 
 const aadNum = (n1, n2) => ({username: "Sammy"})
-console.log(aadNum(1, 2))
+console.log(aadNum())           // { username: 'Sammy' }
