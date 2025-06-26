035d9c733c1c05bbe27b7a6d0806a47a7afd7b88
Somya Keshri <keshrisomya9596@gmail.com>
2025-06-26 23:57:09 +0530

    Nullish Coalescing Operator

--- 04_control_flow/nullishOperator.js
+++ 04_control_flow/nullishOperator.js
@@ -1,6 +1,17 @@
 // Nullish Coalescing Operator (??): null undefined
 
 let val1;
-val1 = 5 ?? 10
+// val1 = 5 ?? 10            // 5
+// val1 = null ?? 10         // 10
+
+// val1 = undefined ?? 15      // 15
+val1 = null ?? 10 ?? 20          // 10
 
 console.log(val1);
+
+
+// TERNARY OPERATOR
+// condition ? true : false
+
+const iceTeaPrice = 100;
+iceTeaPrice <= 80 ? console.log("Less than 80") : console.log("More than 80")
