e9cad53c14526542b120abfb04f4a7c2b0ab93c0
Somya Keshri <keshrisomya9596@gmail.com>
2025-06-27 16:12:34 +0530

    while and do while

--- 05_iterations/02_two.js
+++ 05_iterations/02_two.js
@@ -2,14 +2,14 @@
 
 let i = 0;
 while (i <= 10) {
-    // console.log(`Value of index is ${i}`);
+    console.log(`Value of index is ${i}`);
     i = i + 2;
 }
 
 let myArray = ["flash", "batman", "superman"]
 let arr = 0
 while(arr < myArray.length){
-    // console.log(`Value is ${myArray[arr]}`);
+    console.log(`Value is ${myArray[arr]}`);
     arr = arr + 1;
 }
 
