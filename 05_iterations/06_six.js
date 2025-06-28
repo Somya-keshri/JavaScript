218a34c131522bdf7147bf891ee9d1d11a0a421c
Somya Keshri <keshrisomya9596@gmail.com>
2025-06-28 20:06:37 +0530

    for each

---
+++ 05_iterations/06_six.js
@@ -0,0 +1,29 @@
+const coding = ["js", "ruby", "java","py", "cpp"];
+
+const values = coding.forEach((item) => {
+    console.log(item);                // js ruby java...
+    return item
+} )
+console.log(values)            // undefined
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7];
+// const newNums = myNums.filter( (num) => num > 4 );
+//                      OR
+const newNums = myNums.filter( (num) => {
+    return num > 4;                   
+    // if scope is open then its compulsary to write return keyword
+} );
+console.log(newNums);       // [5, 6, 7]
+
+
+// another method without filter
+const score = [25, 59, 72, 85, 63, 15]
+const Numbers = []
+
+score.forEach( (num) => {
+    if(num > 50){
+        Numbers.push(num);
+    }
+} )
+console.log("Scores: " + Numbers);      // Scores: 59,72,85,63
