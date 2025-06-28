7cbd2b34a356a73f8ff9526b95da25940bd7f22c
Somya Keshri <keshrisomya9596@gmail.com>
2025-06-28 21:58:14 +0530

    for each and filter

--- 05_iterations/06_six.js
+++ 05_iterations/06_six.js
@@ -7,6 +7,7 @@ const values = coding.forEach((item) => {
 console.log(values)            // undefined
 
 
+// FILTER
 const myNums = [1, 2, 3, 4, 5, 6, 7];
 // const newNums = myNums.filter( (num) => num > 4 );
 //                      OR
